98ead3f8ebda9c1db2591629d9ab998753800a8d
Rafael Rosso Coelho <rafaelkrc@hotmail.com>
2022-10-10 07:58:18 +0100

    Ajute da pasta .gitihub

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -5,7 +5,7 @@ def buscar_avatar(usuario):
     """
     Busca o avatar de um usuário no Github
 
-    :param usuario: str com o nome do usuário no github
+    :param usuario: str com o nome do usuário no .github
     :return: str com o link do avatar
     """
     url = f'https://api.github.com/users/{usuario}'
